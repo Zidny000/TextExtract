6115596c079a2327743bd97fc58b95aa0d6c4759
Zidny000 <t.m.zidny@gmail.com>
2025-08-05 17:03:23 +0600

    Update Routes Added

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -6,6 +6,7 @@ from .user_routes import user_routes
 from .api_routes import api_routes
 from .subscription_routes import subscription_routes
 from .stripe_routes import stripe_routes
+from .update_routes import update_routes
 
 def init_routes(app):
     """Initialize routes for the Flask app"""
@@ -13,4 +14,5 @@ def init_routes(app):
     app.register_blueprint(user_routes)
     app.register_blueprint(api_routes)
     app.register_blueprint(subscription_routes)
+    app.register_blueprint(update_routes)
     app.register_blueprint(stripe_routes)
